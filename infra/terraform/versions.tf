05381acc36012a8786989ae80cd925da7f869c13
Murylo Capucho <mcapuchoribeiro@gmail.com>
2023-10-28 01:52:19 -0300

    Implementação da Infra VPC

--- infra/terraform/versions.tf
+++ infra/terraform/versions.tf
@@ -5,4 +5,10 @@ terraform {
       version = "~> 5.17.0"
     }
   }
+
+  backend "s3" {
+    bucket = "mixfast-s3-bucket-vpc"
+    key    = "mixfast-infrastructure"
+    region = "us-east-1"
+  }
 }

bdde6cf598fe728623daa7d0a0131c49927e9fc7
Murylo Capucho <mcapuchoribeiro@gmail.com>
2023-11-05 02:44:01 -0300

    Implementação da Infra VPC

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -54,7 +54,7 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id            = aws_vpc.mixfast_vpc.id
   service_name      = "com.amazonaws.${var.region}.s3"
   vpc_endpoint_type = "Gateway"
-  route_table_ids   = [aws_route_table.mixfast_route_table[0].id]
+  route_table_ids   = [aws_route_table.mixfast_route_table[*].id]
   policy            = data.aws_iam_policy_document.s3_ecr_access.json
 }
 

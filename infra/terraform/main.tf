3b0611b6ec3e1b21d6e81e713fae0e9f0351b241
Murylo Capucho <mcapuchoribeiro@gmail.com>
2023-11-05 02:47:05 -0300

    Implementação da Infra VPC

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -51,10 +51,11 @@ resource "aws_route_table_association" "mixfast_route_table_association_public" 
 }
 
 resource "aws_vpc_endpoint" "s3" {
+  count             = length(var.privatesCIDRblock)
   vpc_id            = aws_vpc.mixfast_vpc.id
   service_name      = "com.amazonaws.${var.region}.s3"
   vpc_endpoint_type = "Gateway"
-  route_table_ids   = [aws_route_table.mixfast_route_table[*].id]
+  route_table_ids   = element(aws_route_table.mixfast_route_table[*].id, count.index)
   policy            = data.aws_iam_policy_document.s3_ecr_access.json
 }
 
